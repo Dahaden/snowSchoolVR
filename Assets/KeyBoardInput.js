cba359cd58addcb4f36fd2faecd42d538e5f94bf
bjon083 <thatbennyguy@gmail.com>
2014-09-04 22:04:11 +1200

    Creating rotational movement for Wii Balance Board.

--- Assets/KeyBoardInput.js
+++ Assets/KeyBoardInput.js
@@ -5,7 +5,11 @@ var maxLean = 10;
 var friction = 0.2;
 var Rsc = 8; // Meters
 
-var slopeAngle : float ;
+// var slopeAngle : float ;
+var slopeAngleSteep : float ;
+var worldPos : Vector3;
+
+var slopeAngle = Quaternion.Euler(Vector3(0, 0, 0));
 
 var buffer = 0.01;
 
@@ -14,57 +18,59 @@ var isColliding = false;
 var terrain : TerrainData;
 
 function Start () {
+	var hitCast : RaycastHit;
+
+    if (Physics.Raycast(transform.position, Vector3.down, hitCast))
+    {
+        Debug.DrawRay(hitCast.point,hitCast.normal * 100,Color.blue);
+
+        var dir = Vector3.up;
+
+        transform.rotation = Quaternion.FromToRotation(dir, hitCast.normal);
+        slopeAngle = transform.rotation;
+    }
+
     var ter : Terrain = GameObject.Find("Terrain").GetComponent(Terrain);
     terrain = ter.terrainData;
-    slopeAngle = terrain.GetSteepness(transform.position.x, transform.position.z);
+    slopeAngleSteep = terrain.GetSteepness(transform.position.x, transform.position.z);
     Debug.Log("Angle: " + slopeAngle);
+    //Debug.Log("AngleSteep: " + slopeAngleSteep);
 }
 
 function Update () {
     if(isColliding) {
-        Debug.Log("IsColliding");
-        // Get Angle of slope
-        slopeAngle = terrain.GetSteepness(transform.position.x, transform.position.z);
-
-        // Get input from keyboard or wiiBoard
+    	Debug.Log("Angle: " + slopeAngle);
         var direction = getVectorInput();
-
-        // Find relative lean angle from -10 -> 10
         var lean : float = maxLean * direction.z;
 
-        // Rotation performed of x axis to make snowboarder appear to lean toe or heel
-        // Needs fixing, needs x and y values
-        var deltaRotation : Quaternion = Quaternion.Euler(Vector3(lean*slopeAngle, 1, 1));
-
-        // Applies lean to board
-        // puts interms of slopeAngle to prevent falling over
-        transform.rotation =  deltaRotation;
-
-        // the direction of a (0, 0, 1) in terms of the global direction
+        var deltaRotation : Quaternion = Quaternion.Euler(Vector3(0, lean, 0));
+        transform.rotation = slopeAngle * deltaRotation;
         var forward = transform.forward;
-        Debug.Log("Velocity X: " + rigidbody.velocity.x);
-        Debug.Log("forward: "+ forward +" and lean: " + lean);
+        //Debug.Log("Velocity X: " + rigidbody.velocity.x);
+        //Debug.Log("forward: "+ forward +" and lean: " + lean);
 
-
-        // Applies for in direction forward at strength of maxLean
-        // Applies more torque around center of gravity dependent on speed
-        //rigidbody.AddForceAtPosition(forward * lean/10000000*-1, new Vector3(rigidbody.velocity.x, 0, 0));
-
-        // Get velocity in terms of local rotation
-        //var velocity = transform.InverseTransformDirection( rigidbody.velocity );//Vector3.RotateTowards(rigidbody.velocity, new Vector3(transform.rotation.x,transform.rotation.y ,transform.rotation.z), 3.0, 0.0);
-
-        // Straighten up velocity system to align with character
-
-        // If Leaning
-        // if (Mathf.Abs(lean) < buffer) {
-        //     // Change velocity from z to x by 50%
-        //     rigidbody.velocity = transform.TransformDirection(new Vector3(velocity.x + velocity.z * 0.5, 0, velocity.z * 0.5));
-        // } else {
-        //     // Change velocity from z to x by 10%
-        //     rigidbody.velocity = transform.TransformDirection(new Vector3(velocity.x + velocity.z * 0.1, 0, velocity.z * 0.9));
-        // }
-    }
-
+        rigidbody.AddForceAtPosition(forward * lean/10000000*-1, new Vector3(rigidbody.velocity.x, 0, 0));
+        var velocity = transform.InverseTransformDirection( rigidbody.velocity );//Vector3.RotateTowards(rigidbody.velocity, new Vector3(transform.rotation.x,transform.rotation.y ,transform.rotation.z), 3.0, 0.0);
+        
+         if (Mathf.Abs(lean) < buffer) {
+             // Change velocity from z to x by 50%
+             rigidbody.velocity = transform.TransformDirection(new Vector3(velocity.x + velocity.z * 0.5, 0, velocity.z * 0.5));
+         } else {
+             // Change velocity from z to x by 10%
+             rigidbody.velocity = transform.TransformDirection(new Vector3(velocity.x + velocity.z * 0.1, 0, velocity.z * 0.9));
+             Debug.Log("test");
+        }
+        
+        rigidbody.AddForce(transform.TransformDirection(new Vector3(-1000, 0, 0)));
+        //var eulerAngleVelocity : Vector3 = Vector3 (0, 100, 0);
+        
+        if (lean >= 0.5) {
+        	//var deltaRotation : Quaternion = Quaternion.Euler(eulerAngleVelocity * Time.deltaTime);
+        	// rigidbody.MoveRotation(rigidbody.rotation * deltaRotation);
+        } else if (lean <= -0.5) {
+        	// rigidbody.AddForce(transform.TransformDirection(Vector3.left * 10));
+        }
+    }		
 }
 
 function OnCollisionEnter (col : Collision)
@@ -78,11 +84,11 @@ function OnCollisionExit (col : Collision)
 }
 
 function getVectorInput() {
-    var left_toe = Input.GetKey(KeyCode.W);
-    var left_heel = Input.GetKey(KeyCode.S);
+    var left_toe = Input.GetKey(KeyCode.S);
+    var left_heel = Input.GetKey(KeyCode.D);
 
-    var right_toe = Input.GetKey(KeyCode.E);
-    var right_heel = Input.GetKey(KeyCode.D);
+    var right_toe = Input.GetKey(KeyCode.W);
+    var right_heel = Input.GetKey(KeyCode.E);
 
     var x = 0.0;
     var z = 0.0;
@@ -90,29 +96,29 @@ function getVectorInput() {
     if (left_toe) {
 
         x -= 0.5;
-        z += 0.5;
-        //Debug.Log("left_toe: x-" + x +", y-" + y);
+        z -= 0.5;
+        //Debug.Log("left_toe: x-" + x +", z-" + z);
     }
 
     if (left_heel) {
 
         x -= 0.5;
-        z -= 0.5;
-        //Debug.Log("left_heel: x-" + x +", y-" + y);
+        z += 0.5;
+        //Debug.Log("left_heel: x-" + x +", z-" + z);
     }
 
     if (right_toe) {
 
         x += 0.5;
-        z += 0.5;
-        //Debug.Log("right_toe: x-" + x +", y-" + y);
+        z -= 0.5;
+        //Debug.Log("right_toe: x-" + x +", z-" + z);
     }
 
     if (right_heel) {
 
         x += 0.5;
-        z -= 0.5;
-        //Debug.Log("right_heel: x-" + x +", y-" + y);
+        z += 0.5;
+        //Debug.Log("right_heel: x-" + x +", z-" + z);
     }
 
     //Debug.Log("X: " + x + ", Y: " + y);
