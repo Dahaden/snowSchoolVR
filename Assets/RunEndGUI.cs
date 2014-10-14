158b6ffb0f505362460a005d7e60bdccbdabbd67
Sara Metz <sara@smetz.co.nz>
2014-10-14 14:45:02 +1300

    Added end game message

--- Assets/RunEndGUI.cs
+++ Assets/RunEndGUI.cs
@@ -8,6 +8,9 @@ public class RunEndGUI : VRGUI
 		public int timeLeft;
 		public bool playbackNext;
 		public bool endOfPlayback;
+		public bool runTimeEnd;
+		public bool gameTimeEnd;
+		private string labelText;
 
 		public override void OnVRGUI ()
 		{
@@ -17,16 +20,32 @@ public class RunEndGUI : VRGUI
 		
 				GUI.Label (new Rect (Screen.width / 4 - 300, 200, 1200f, 100f), "WELL DONE!");
 
-				string text = "";
-				if (playbackNext) {
-						text = "You reached the bottom of the mountain. You will be shown a playback of your run in:";
+				labelText = "";
+
+				if (gameTimeEnd) {
+						labelText = "You have reached the end of this game! The game will close in:";
 				} else if (endOfPlayback) {
-						text = "You have reached the end of the playback. Your next run will begin in:";
+						labelText = "You have reached the end of the playback. Your next run will begin in:";
+				} else if (playbackNext && !runTimeEnd) {
+						labelText = "You reached the bottom of the mountain. You will be shown a playback of your run in:";
+				} else if (playbackNext && runTimeEnd) {
+						labelText = "Your run timed out. You will be shown a playback of your run in:";
+				} else if (runTimeEnd) {
+						labelText = "Your run timed out. The next run will begin in:";
 				} else {
-						text = "You reached the bottom of the mountain. The next run will begin in:";
+						labelText = "You reached the bottom of the mountain. The next run will begin in:";
 				}
-				GUI.Label (new Rect (Screen.width / 4 + 50, 250, 500f, 120f), text);
+
+				GUI.Label (new Rect (Screen.width / 4 + 50, 250, 500f, 120f), labelText);
 
 				GUI.Label (new Rect (Screen.width / 4 - 300, 350, 1200, 100), timeLeft.ToString ());        
+		}
+
+		public void resetBools ()
+		{
+				playbackNext = false;
+				endOfPlayback = false;
+				runTimeEnd = false;
+				gameTimeEnd = false;
 		}
 }
