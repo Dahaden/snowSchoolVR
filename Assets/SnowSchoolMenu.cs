28eb8fffc83279c3c5f7fec9a32c8f9238ac07f1
Sara Metz <sara@smetz.co.nz>
2014-10-25 10:04:14 +1300

    Pushing latest binaries to see if others can run

--- Assets/SnowSchoolMenu.cs
+++ Assets/SnowSchoolMenu.cs
@@ -15,7 +15,7 @@ public class SnowSchoolMenu : VRGUI
 
 				GUI.Label (new Rect (Screen.width / 4 - 300, 200, 1200f, 100f), "SNOW SCHOOL VR");
 				
-				string promptStr = showBreak ? "Good job! You have completed the first half of this demo. Take a break, grab some chocolate and we'll tell you what will happen next." : "Use the balance board to control your movement as if you are snowboarding.\nThe aim of this exercise is to move left and right to touch the green lines on the snow ";
+				string promptStr = showBreak ? "Good job! You have completed the first half of this demo. Take a break, grab some chocolate and we'll tell you what will happen next." : "Use the balance board to control your movement as if you are snowboarding.\nThe aim of this exercise is to move left and right to touch the red lines on the snow ";
 				GUI.Label (new Rect (Screen.width / 4 + 50, 250, 500f, 120f), promptStr);
 
 				GUI.DrawTexture (new Rect (Screen.width / 2 - 125, 380, 230f, 128f), lineImage, ScaleMode.StretchToFill);
